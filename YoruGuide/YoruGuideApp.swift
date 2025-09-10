160e9be3ea6eaf01a5a0060d20e9e6880e6a3ffe
jimbookshkust <khhungac@connect.ust.hk>
2025-09-10 12:11:27 +0800

    Initial Commit

---
+++ YoruGuide/YoruGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoruGuideApp.swift
+//  YoruGuide
+//
+//  Created by jimbook on 10/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct YoruGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YoruGuideTests/YoruGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  YoruGuideTests.swift
+//  YoruGuideTests
+//
+//  Created by jimbook on 10/9/2025.
+//
+
+import Testing
+@testable import YoruGuide
+
+struct YoruGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
