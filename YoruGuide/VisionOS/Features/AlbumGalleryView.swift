5f3d4ec83221f25a9166b173bdb04b4390d971bf
jimbookshkust <khhungac@connect.ust.hk>
2025-09-24 02:57:00 +0800

    Added visionOS entry point

---
+++ YoruGuide/VisionOS/Features/AlbumGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumGalleryView.swift
+//  YoruGuide
+//
+//  Created by jimbook on 24/9/2025.
+//
+
+import SwiftUI
+
+struct AlbumGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumGalleryView()
+}
