ef48d0f1ce5c5ef2300c7510cb201278bb42119b
jimbookshkust <khhungac@connect.ust.hk>
2025-10-15 00:42:52 +0800

    Initial Commit

---
+++ YoruGuide/YoruGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoruGuideApp.swift
+//  YoruGuide
+//
+//  Created by jimbook on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct YoruGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
